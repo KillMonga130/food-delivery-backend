332c5d6afef2b4d4e6000b9e04b6039abbb4285e
KillMonga130 <mubvafhimoses813@gmail.com>
2024-07-19 14:15:13 +0200

    Install development dependencies

--- config/db.js
+++ config/db.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:27017/foodDeliveryApp', {
+        await mongoose.connect(db, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useCreateIndex: true,
